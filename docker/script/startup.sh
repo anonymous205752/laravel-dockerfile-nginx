73281db0067c8a13b03e4bf9b13ce6d16a6aa75c
anonymous205752 <you@example.com>
2025-08-04 01:59:04 +0100

    Fix startup script path and content

--- docker/script/startup.sh
+++ docker/script/startup.sh
@@ -1,29 +1,29 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 main() {
     setup_environment
 }
 
-# Set up the environment: run migrations, optimize, etc.
 setup_environment() {
     echo "Setting up the environment..."
     grant_execute_permission
     wait_for_db
     clear_and_optimize_app
     run_migrations
-    # run_npm_build
+    # run_npm_build  # Uncomment if needed
     start_supervisor
 }
 
-# Ensure permissions for ./artisan
 grant_execute_permission() {
     chmod a+x ./artisan
 }
 
-# Wait for the database to be ready using netcat (nc)
 wait_for_db() {
     MAX_RETRIES=30
     COUNT=0
+    DB_HOST=${DB_HOST:-db}
+    DB_PORT=${DB_PORT:-5432}
+
     echo "Waiting for database connection at $DB_HOST:$DB_PORT..."
 
     until nc -z "$DB_HOST" "$DB_PORT"; do
@@ -39,13 +39,11 @@ wait_for_db() {
     echo "✅ Database Connected"
 }
 
-# Run Laravel migrations
 run_migrations() {
     echo "Running Laravel migrations..."
     ./artisan migrate --force
 }
 
-# Clear and optimize Laravel cache
 clear_and_optimize_app() {
     echo "Clearing and optimizing Laravel caches..."
     ./artisan config:clear
@@ -56,21 +54,6 @@ clear_and_optimize_app() {
     ./artisan optimize
 }
 
-# Build web apps (Uncomment in setup_environment if needed for web apps)
-run_npm_build() {
-    if [ -f "package.json" ]; then
-        if [ ! -d "node_modules" ]; then
-            echo "Running NPM clean install"
-            npm ci
-        fi
-        echo "Running NPM build"
-        npm run build
-    else
-        echo "No package.json found, skipping NPM build"
-    fi
-}
-
-# Start Supervisor
 start_supervisor() {
     echo "Starting Supervisor..."
     exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
